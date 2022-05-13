f4a5c00a424fb409cfdcf6faec00cf7fd3ec0b3d
VladimirKrivko <mark.tatyschev@gmail.com>
2022-05-14 01:30:15 +0300

    переименовал ReadMe.txt в ReadMe.md и добавил техническую информацию

--- src/main/java/com/example/gui_cryptonalyzer/MainApplication.java
+++ src/main/java/com/example/gui_cryptonalyzer/MainApplication.java
@@ -4,12 +4,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class MainApplication extends Application {
     @Override
